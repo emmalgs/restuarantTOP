01466d2907a865509dbbaf2d8043125e46358040
Emma Gerigscott <elgerig@gmail.com>
2022-11-23 15:00:06 -0800

    styling menu

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -11,8 +11,7 @@ export function menu() {
     const CONTACT = document.createElement('div')
     const MAIN = document.createElement('div')
     const DINNER = document.createElement('div')
-    const menuItem = document.createElement('div')
-    const price = document.createElement('div')
+    
     const menuPic = document.createElement('img')
 
     SIDEBAR.classList.add('sidebar')
@@ -35,4 +34,26 @@ export function menu() {
 
     MAIN.setAttribute('id', 'main')
     CONTENT.append(MAIN)
+
+    DINNER.classList.add('dinner-header')
+    DINNER.textContent = 'DINNER'
+    MAIN.append(DINNER)
+    
+    //menu items
+
+    const menuItem1 = document.createElement('div')
+    const menuItem1_desc = document.createElement('div')
+    const price1 = document.createElement('div')
+
+    menuItem1.classList.add('menu-item')
+    menuItem1.textContent = 'Timothy Western Hay'
+    MAIN.append(menuItem1)
+
+    menuItem1_desc.classList.add('menu-desc')
+    menuItem1_desc.textContent = 'Sun-dried on the plains of Northern California for the freshest grassy chew'
+    menuItem1.append(menuItem1_desc)
+
+    price1.classList.add('price')
+    price1.textContent = '$9.99'
+    MAIN.append(price1)
 }
