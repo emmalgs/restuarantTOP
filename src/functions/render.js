dcd928ef8fdcb4db6c107eb41831fc1e38802ca4
Emma Gerigscott <elgerig@gmail.com>
2022-11-23 13:26:01 -0800

    Well. My images links were broken

--- src/functions/render.js
+++
@@ -1,7 +0,0 @@
-export function render() {
-    const CONTENT = document.querySelector('#content');
-    CONTENT.style='';
-    while(CONTENT.children.length > 1){
-        CONTENT.children[1].remove();
-    }
-}
--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
 import './style.css'
 import { pageload } from './functions/pageload.js'
-import { menu } from './pages/menu.js'
-import { header } from './pages/header'
-import { home } from './pages/home'
-import { render } from './functions/render'
 
 function component() {
-    render()
-    document.querySelector('.header').addEventListener('click', () => {
-        render();
-        pageload();
-    })
-    document.querySelector('.menu').addEventListener('click', () => {
-        render();
-        menu();
-    })
+ pageload()
+ document.querySelector('.quote').textContent = 'THE HELL'
 }
 
 component()
